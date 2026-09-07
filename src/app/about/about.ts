24d8ba457e42851e3fd99e41d002e075d62c169b
Jose Forero Angel <josem.foreroa@utadeo.edu.co>
2026-09-07 09:34:07 -0500

    Landing page done

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { GridSquare } from '../grid-square/grid-square';
 
 @Component({
   selector: 'app-about',
-  imports: [GridSquare],
+  imports: [GridSquare, RouterLink],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
 export class About {}
-
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { About } from './about/about';
+import { Landing } from './landing/landing';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'about', pathMatch: 'full' },
     { path: 'about', component: About },
+    { path: 'main', component: Landing }
 ];
